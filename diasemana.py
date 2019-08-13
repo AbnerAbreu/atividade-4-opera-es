05a7a65a7c0df85f46998a8c12d1c4d8dc360a51
AbnerAbreu <abnerdrift94@gmail.com>
2019-08-13 17:30:10 -0300

    Adicionar indentação/Dia-Da-Semana

--- diasemana.py
+++ diasemana.py
@@ -1,5 +1,11 @@
 
-diasemana = {1:'domingo',2:'segunda',3:'terça',4:'quarta',5:'quinta',6:'sexta',7:'sabado'}
+diasemana = {1:'domingo',
+             2:'segunda',
+             3:'terça',
+             4:'quarta',
+             5:'quinta',
+             6:'sexta',
+             7:'sabado'}
 
 dia = int(input('digite um numero: '))
 
